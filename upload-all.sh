b71b720ee5027cfb6a5912cb1f58f6f6a8ccc5ce
fred231084g <freddy1984govoni@gmail.com>
2023-01-03 03:34:01 +0100

    Delete upload-all.sh

--- upload-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-scp ./releases/wikipedia-*-beta*apk releases1002.eqiad.wmnet:/srv/org/wikimedia/releases/mobile/android/wikipedia/betas/
-scp ./releases/wikipedia-*-r*apk releases1002.eqiad.wmnet:/srv/org/wikimedia/releases/mobile/android/wikipedia/stable/
